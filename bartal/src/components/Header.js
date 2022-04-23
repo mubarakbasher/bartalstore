4747e00984bfe376eefc8d6702c2385dd86cef79
Mubarak Basher <mubarakwadi360@gmail.com>
2022-04-23 14:31:28 +0200

    try to fix problem

---
+++ bartal/src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Logo from './Logo'
+import Menu from './Menu'
+
+const Header = () => {
+  return (
+    <div>
+        <Logo/>
+        <Menu/>
+    </div>
+  )
+}
+
+
+
+export default Header
